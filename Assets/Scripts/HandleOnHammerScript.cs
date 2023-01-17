d7768eadb39efdba2e194b8eae6a2fcf13b5992b
roman <roman.billeter@gmail.com>
2023-01-17 17:17:52 +0100

    zwischenspeichern

--- Assets/Scripts/HandleOnHammerScript.cs
+++ Assets/Scripts/HandleOnHammerScript.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRfreePluginUnity;
 
 public class HandleOnHammerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject hammerheadCollider;
+    public GameObject newHammer;
 
     void OnTriggerEnter(Collider other)
     {
-       
-        Debug.Log("object entered trigger");
-        Debug.Log(other);
-        Debug.Log(other.tag);
-        if(other.CompareTag(hammerheadCollider.tag))
-        {Debug.Log("Hammerhead correctly detected");}
+        if (other.CompareTag(hammerheadCollider.tag)) 
+        { Debug.Log("Hammerhead correctly detected"); }
+
+        GameObject handle = gameObject.transform.parent.gameObject;
+        GameObject hammer = handle.transform.parent.gameObject;
+        //GameObject hammerHead = hammerheadCollider.transform.parent.gameObject;
+        hammer.SetActive(false);
+        for (int i = 0; i < newHammer.transform.childCount; i++)
+        {
+            newHammer.transform.GetChild(i).transform.position = hammer.transform.GetChild(i).transform.position;
+            newHammer.transform.GetChild(i).transform.rotation = hammer.transform.GetChild(i).transform.rotation;
+
+        }
+        
+        
+        
         
     }
+    
+    
 }
