1aa0baf60d920cb0255d1cae0a320b1a757e91ab
roman <roman.billeter@gmail.com>
2023-01-24 16:05:28 +0100

    added mtm for reach grasb basic

--- Assets/Scripts/ObjectInteractions.cs
+++ Assets/Scripts/ObjectInteractions.cs
@@ -1,28 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectInteractions : MonoBehaviour
 {
     public List<GameObject> currentlyGraspedLH = new List<GameObject>();
     public List<GameObject> currentlyGraspedRH = new List<GameObject>();
     public GameObject debugObj;
+    public GameObject TranscriptionDisplay;
     // Start is called before the first frame update
     void Start()
     {
         
     }
     
-    void addGraspedObject(GameObject graspedObj,bool isRightHand)
+    public void addGraspedObject(GameObject graspedObj,bool isRightHand)
     {
-        if (isRightHand) {currentlyGraspedRH.Add(graspedObj); }
-        else { currentlyGraspedLH.Add(graspedObj); }
+        if (isRightHand)
+        {
+            currentlyGraspedRH.Add(graspedObj);
+            TranscriptionDisplay.transform.GetChild(0).GetComponent<Text>().text += "\n R \n G";
+        }
+        else
+        {
+            currentlyGraspedLH.Add(graspedObj); 
+            TranscriptionDisplay.transform.GetChild(1).GetComponent<Text>().text += "\n R \nG";
+        }
     }
     
-    void removeGraspedObj(GameObject graspedObj,bool isRightHand)
+    public void removeGraspedObj(GameObject graspedObj,bool isRightHand)
     {
-        if (isRightHand) {currentlyGraspedRH.Remove(graspedObj); }
-        else { currentlyGraspedLH.Remove(graspedObj); }
+        if (isRightHand)
+        {
+            currentlyGraspedRH.Remove(graspedObj); 
+            TranscriptionDisplay.transform.GetChild(0).GetComponent<Text>().text += "\n RL";
+        }
+        else
+        {
+            currentlyGraspedLH.Remove(graspedObj); 
+            TranscriptionDisplay.transform.GetChild(1).GetComponent<Text>().text += "\n RL";
+        }
     }
 
     // Update is called once per frame
