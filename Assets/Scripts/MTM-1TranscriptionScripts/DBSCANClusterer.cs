33a00127636ed07fdde4a95582c60c7ee17a069a
Roman <romanbi@student.ethz.ch>
2023-02-20 12:37:00 +0100

    added visual dbsort

--- Assets/Scripts/MTM-1TranscriptionScripts/DBSCANClusterer.cs
+++ Assets/Scripts/MTM-1TranscriptionScripts/DBSCANClusterer.cs
@@ -5,9 +5,76 @@ using UnityEngine;
 public class DBSCANClusterer : MonoBehaviour {
     public float epsilon = 0.05f;  // Radius around each point that determines its neighborhood
     public int minPoints = 5;  // Minimum number of points required to form a dense region
-    public GameObject obj1;
-    public GameObject obj2;
 
+    public float rad = 0.1f;
+    public GameObject spherePrefab;
+    
+    
+    public List<GameObject> dataVisualisationObjects = new List<GameObject>();
+
+    private Vector3[] posArray = new []
+    {
+        new Vector3(0, 0, 0),
+        new Vector3(1, 0, 0),
+        new Vector3(0, 1, 0),
+        new Vector3(1, 1, 0)
+    };
+
+    private int[] groupIDs = new[] { 1, 1, 2, 2 };
+    private int nrOfGroups = 2;
+
+    public int[][] colours = new int[][]
+    {
+        new int[] { 255, 0, 0 },
+        new int[] { 255, 255, 0 },
+        new int[] { 0, 234, 255 },
+        new int[] { 170, 0, 255 },
+        new int[] { 255, 127, 0 },
+        new int[] { 191, 255, 0 },
+        new int[] { 0, 149, 255 },
+        new int[] { 255, 0, 170 },
+        new int[] { 255, 212, 0 },
+        new int[] { 106, 255, 0 },
+        new int[] { 0, 64, 255 }
+    };
+
+
+    void drawData()
+    {
+        float radius = rad;
+        foreach(GameObject obj in dataVisualisationObjects)
+        {
+            Destroy(obj);
+        }
+        dataVisualisationObjects.Clear();
+        Material[] materials = new Material[nrOfGroups];
+        for (int i = 0; i < nrOfGroups; i++)
+        {
+         materials[i] = new Material(Shader.Find("Standard"));
+         materials[i].color = new Color(colours[(int)Mathf.Repeat(i, colours.Length)][0],
+             colours[(int)Mathf.Repeat(i, colours.Length)][1], colours[(int)Mathf.Repeat(i, colours.Length)][2]);
+        }
+        
+        for (int i = 0; i < posArray.Length; i++)
+        {
+            // Instantiate the sphere prefab at the position of the sphere data
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.localScale = Vector3.one * radius;
+            sphere.transform.position = posArray[i];
+            dataVisualisationObjects.Add(sphere);
+
+            // Set the color of the sphere based on the group identifier
+            if (groupIDs[i]>0)
+            {sphere.GetComponent<Renderer>().material = materials[groupIDs[i] - 1];}
+        }
+    }
+
+    private void Start()
+    {
+        rad = 0.1f;
+        drawData();
+        
+    }
 
     private void Update()
     {
@@ -15,9 +82,9 @@ public class DBSCANClusterer : MonoBehaviour {
         {
             Cluster(GameObject.Find("Recorder/Player").GetComponent<HandPoseManipulation>().rPosArray);
         }
-        if (Input.GetKeyDown("l"))
+        if (Input.GetKeyDown("m"))
         {
-            Debug.Log(Vector3.Distance(obj1.transform.position,obj2.transform.position));
+            drawData();
         }
     }
 
@@ -95,5 +162,6 @@ public class DBSCANClusterer : MonoBehaviour {
             }
         }
     }
+    
 }
 
